f825a10bf67128b0e298829f221249237aae3ee1
mimin5 <azizahichabts@gmail.com>
2022-02-09 14:37:48 +0800

    wpu ep3

--- crud-app/routes/web.php
+++ crud-app/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about',[
+        "name" => "azizah khairun nisa",
+        "email" => "azizahichabts@gmail.com",
+        "image" => "1.png"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
